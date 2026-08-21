3226a58460370405b86f4d398cab0231cbf9b5a2
Ralph Göstenmeier <r14r@via-internet.de>
2026-08-21 15:47:07 +0200

    Route project doctor and hook commands

--- cmd/git-cli/main.go
+++ cmd/git-cli/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
-	"git-cli/internal/app"
 	"os"
+
+	"git-cli/internal/app"
+	"git-cli/internal/doctor"
+	"git-cli/internal/precommitcmd"
+	"git-cli/internal/projectcmd"
 )
 
 var version = "dev"
 
 func main() {
 	app.Version = version
-	os.Exit(app.New().Run(os.Args[1:]))
+	args := os.Args[1:]
+	if len(args) > 0 {
+		switch args[0] {
+		case "precommit":
+			os.Exit(precommitcmd.Run(args[1:], os.Stdout, os.Stderr))
+		case "project":
+			os.Exit(projectcmd.Run(args[1:], os.Stdout, os.Stderr))
+		case "doctor":
+			os.Exit(doctor.Run(os.Stdout, os.Stderr))
+		case "hook":
+			if len(args) == 3 && args[1] == "run" && args[2] == "pre-commit" {
+				code := app.New().Run([]string{"security", "check-staged"})
+				if code != 0 {
+					os.Exit(code)
+				}
+				os.Exit(precommitcmd.Run([]string{"run"}, os.Stdout, os.Stderr))
+			}
+		}
+	}
+	os.Exit(app.New().Run(args))
 }
